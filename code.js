68506729ab286da7aff248593a24f727c8078094
weworkcode <90383460+weworkcode@users.noreply.github.com>
2022-03-01 11:07:29 -0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,12 @@
+public class MyInitials {
+     public static void main(String[] args){
+         System.out.println("    j");
+         System.out.println("    j");
+         System.out.println("    j");
+         System.out.println("    j");
+         System.out.println("    j");
+         System.out.println("j   j");
+         System.out.println("j   j");
+         System.out.println(" jjj");
+     }
+}
